205987fe5eac3e47ea41d1358c746b798181834b
michelaWork <101396014+michelaWork@users.noreply.github.com>
2023-06-16 11:37:55 +0200

    post page created

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Post = require("../models/Post");
 
 //Routes
+//get home
 router.get("", async (req, res) => {
   const locals = {
     title: "NodeJs Blog",
@@ -19,6 +20,22 @@ router.get("", async (req, res) => {
 
 router.get("/about", (req, res) => {
   res.render("about");
+});
+
+//get :id for single post page
+router.get("/post/:id", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Personal Blog created with NodeJs, Express and MongoDB",
+    };
+
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
