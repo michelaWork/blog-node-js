d4954bd39724a85eac1e89bc1bff63eed7e67c92
michelaWork <101396014+michelaWork@users.noreply.github.com>
2023-06-19 09:41:24 +0200

    sign in and validation functionality

--- public/js/script.js
+++ public/js/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
       searchInput.focus();
     });
   }
-  searchClose.addEventListener("click", function () {
+  searchClose.addEventListener("click", function() {
     searchBar.style.visibility = "hidden";
     searchBar.classList.remove("open");
     this.setAttribute("aria-expanded", "true");
